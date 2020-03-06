3c4f90e1eff4874dc7640edaec7085a7df176b8a
Julius <Julius>
2020-03-06 09:37:12 +0200

    Loop

--- js/funkcijos.js
+++ js/funkcijos.js
@@ -12,8 +12,28 @@ function daugyba( num1, num2) {
     console.log('dave:', num1, num2)
     return num1 * num2;
 }
-
-
 console.log( daugyba( skaicius1, skaicius2 ) );
 console.log( daugyba( skaicius3, skaicius2 ) );
 console.log( daugyba( skaicius1, skaicius3 ) );
+
+console.log('-------------------');
+
+let arr = [];
+let count = '';
+
+for (let i = 0; i < 17; i++) {
+    arr += i;
+
+    // console.log(i);
+    if (i % 2 == 1) {
+        console.log('Julius');
+    } else {
+        console.log(i);
+    }
+    count++;
+} 
+console.log('-------------------');
+// console.log(arr.length);
+console.log('Veiksmu:' + count);
+
+
