3f83021be7b9f030405561ff4197d80f0c9b2c23
Julius <Julius>
2020-03-06 10:25:27 +0200

    Test

--- js/funkcijos.js
+++ js/funkcijos.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 const skaicius1 = 2;
 const skaicius2 = -8;
 const skaicius3 = 5;
@@ -8,32 +7,31 @@ const skaicius3 = 5;
 
 
 
-function daugyba( num1, num2) {
-    console.log('dave:', num1, num2)
-    return num1 * num2;
-}
-console.log( daugyba( skaicius1, skaicius2 ) );
-console.log( daugyba( skaicius3, skaicius2 ) );
-console.log( daugyba( skaicius1, skaicius3 ) );
-
-console.log('-------------------');
-
-let arr = [];
-let count = '';
-
-for (let i = 0; i < 17; i++) {
-    arr += i;
-
-    // console.log(i);
-    if (i % 2 == 1) {
-        console.log('Julius');
-    } else {
-        console.log(i);
+function daugyba( num1, num2) { 
+    if ( typeof(num1) !== 'number' ) {
+        return  console.error('Klaida (1)');
     }
-    count++;
-} 
-console.log('-------------------');
-// console.log(arr.length);
-console.log('Veiksmu:' + count);
+    if ( num1 === Infinity || num1 === Infinity ) {
+        return console.error('Klaida (0)');     
+    }
+    if ( num2 === Infinity || num2 === -Infinity ) {
+        return console.error('Klaida (3)');     
+    }
+    if ( typeof(num2) !== 'number' ) {
+        return console.error('Klaida (2)');
+    }
+    if ( ''+num1 === 'NaN' ) {
+        return console.error('Klaida (4)');
+    }
+    const anw = num1 * num2;
+    return console.log(anw);
+}
+daugyba( skaicius1, skaicius2 );
+daugyba( skaicius3, skaicius2 );
+daugyba( skaicius1, skaicius3 );
 
 
+daugyba( '12', 1 );
+daugyba( NaN, 0 );
+
+// daubgyba( infinity, 855 );
--- js/main-2.js
+++ js/main-2.js
@@ -212,6 +212,42 @@ console.log('cont:' + count);
 
 
 
+// const skaicius1 = 2;
+// const skaicius2 = -8;
+// const skaicius3 = 5;
+
+
+
+
+// function daugyba( num1, num2) {
+//     console.log('dave:', num1, num2)
+//     return num1 * num2;
+// }
+// console.log( daugyba( skaicius1, skaicius2 ) );
+// console.log( daugyba( skaicius3, skaicius2 ) );
+// console.log( daugyba( skaicius1, skaicius3 ) );
+
+// console.log('-------------------');
+
+// let arr = [];
+// let count = '';
+
+// for (let i = 0; i < 17; i++) {
+//     arr += i;
+
+//     // console.log(i);
+//     if (i % 2 == 1) {
+//         console.log('Julius');
+//     } else {
+//         console.log(i);
+//     }
+//     count++;
+// } 
+// console.log('-------------------');
+// console.log('Veiksmu:' + count);
+
+
+
 
 
 
